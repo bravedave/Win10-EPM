1532682c57f14c9ff9cce128423213cf0086cb9a
Dave Bray <david@brayworth.com.au>
2019-10-24 14:04:21 +1000

    report order

--- wepm/dao/wepm_event.php
+++ wepm/dao/wepm_event.php
@@ -20,16 +20,18 @@ class wepm_event extends _dao {
                 we1.*
             FROM
                 wepm_event we1
-                JOIN (SELECT
-                            locale, MAX(created) created
-                        FROM
-                            wepm_event
-                        WHERE
-                            created >= "%s"
-                        GROUP BY
-                            locale) we2
-                        ON we1.locale = we2.locale
-                        AND we1.created = we2.created', date('Y-m-d', strtotime( $date)));
+                    JOIN
+                (SELECT
+                    locale, MAX(created) created
+                FROM
+                    wepm_event
+                WHERE
+                    created >= "%s"
+                GROUP BY
+                    locale) we2
+            ON we1.locale = we2.locale
+                AND we1.created = we2.created
+            ORDER BY created ASC', date('Y-m-d', strtotime( $date)));
 
         if ( $res = $this->Result($sql)) {
             return $res->dtoSet();
