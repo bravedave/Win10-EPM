730a589d4bb6713061825087000425248d224706
Dave Bray <david@brayworth.com.au>
2022-04-30 15:55:52 +1000

    deprecation warnings

--- wepm/dao/wepm_event.php
+++ wepm/dao/wepm_event.php
@@ -4,42 +4,39 @@
  * BrayWorth Pty Ltd
  * e. david@brayworth.com.au
  *
- * This work is licensed under a Creative Commons Attribution 4.0 International Public License.
- *      http://creativecommons.org/licenses/by/4.0/
+ * MIT License
  *
 */
 
 namespace dvc\wepm\dao;
-use dao\_dao;
+
+use dvc\dao\_dao;
 
 class wepm_event extends _dao {
-    protected $_db_name = 'wepm_event';
+  protected $_db_name = 'wepm_event';
 
-    function getUniqueSince( string $date) : array {
-        $sql = sprintf('SELECT
-                we1.*
-            FROM
-                wepm_event we1
-                    JOIN
-                (SELECT
-                    locale, MAX(created) created
-                FROM
-                    wepm_event
-                WHERE
-                    created >= "%s"
-                GROUP BY
-                    locale) we2
-            ON we1.locale = we2.locale
-                AND we1.created = we2.created
-            ORDER BY created ASC', date('Y-m-d', strtotime( $date)));
+  function getUniqueSince(string $date): array {
+    $sql = sprintf('SELECT
+        we1.*
+      FROM
+        wepm_event we1
+          JOIN
+        (SELECT
+          locale, MAX(created) created
+        FROM
+          wepm_event
+        WHERE
+          created >= "%s"
+        GROUP BY
+          locale) we2
+      ON we1.locale = we2.locale
+        AND we1.created = we2.created
+      ORDER BY created ASC', date('Y-m-d', strtotime($date)));
 
-        if ( $res = $this->Result($sql)) {
-            return $res->dtoSet();
-
-        }
-
-        return [];
-
+    if ($res = $this->Result($sql)) {
+      return $res->dtoSet();
     }
 
+    return [];
+  }
 }
