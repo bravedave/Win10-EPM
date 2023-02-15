794114f235db78034e81b52e1cd99fc1d2d955f9
Dave Bray <david@brayworth.com.au>
2023-02-15 12:44:03 +1000

    compatibility tweak

--- wepm/controller.php
+++ wepm/controller.php
@@ -18,52 +18,43 @@ use strings;
 use sys;
 
 class controller extends \Controller {
+
 	protected function _index() {
-		$seed = strtotime( '-7 days');
+		$seed = strtotime('-7 days');
 		$date = date('Y-m-d', $seed);
 
 		$dao = new dao\wepm_event;
 		$this->data = (object)[
-			'title' => sprintf( 'WEPM Log : since %s', strings::asLocalDate( $date)),
-			'dtoSet' => $dao->getUniqueSince( $date)
-
+			'title' => sprintf('WEPM Log : since %s', strings::asLocalDate($date)),
+			'dtoSet' => $dao->getUniqueSince($date)
 		];
 
 		$this->render([
-			'meta' => [ sprintf( '<meta http-equiv="refresh" content="300; url=%s" />', strings::url( $this->route))],
+			'meta' => [sprintf('<meta http-equiv="refresh" content="300; url=%s" />', strings::url($this->route))],
 			'title' => $this->title = $this->label,
 			'primary' => 'report',
-			'secondary' =>'index'
-
+			'secondary' => 'index'
 		]);
-
 	}
 
 	protected function before() {
+
 		config::wepm_checkdatabase();
 		parent::before();
-
-	}
-
-	protected function getView( $viewName = 'index', $controller = null, $logMissingView = true) {
-		$view = sprintf( '%s/views/%s.php', __DIR__, $viewName );		// php
-		if ( file_exists( $view))
-			return ( $view);
-
-		return parent::getView( $viewName, $controller, $logMissingView);
-
+		$this->viewPath[] = __DIR__ . '/views/';
 	}
 
 	protected function posthandler() {
+
 		$action = $this->getPost('action');
 
-		if ( 'wemp-log' == $action) {
+		if ('wemp-log' == $action) {
 			/**
 			 * curl -X POST -H "Accept: application/json" -d action=wemp-log -d key="-- some random password --" -d locale="COMPUTE-11" "http://localhost/"
 			 */
 			$key = $this->getPost('key');
 			// REVIEW : Should get a password thing happening here
-			if ( '-- some random password --' == $key) {
+			if ('-- some random password --' == $key) {
 				$a = [
 					'created' => \db::dbTimestamp(),
 					'updated' => \db::dbTimestamp(),
@@ -87,60 +78,51 @@ class controller extends \Controller {
 				];
 
 				$dao = new dao\wepm_event;
-				$dao->Insert( $a);
-
+				$dao->Insert($a);
 			}
 
-			Json::ack( $action);
+			Json::ack($action);
 			return;	// because validation is disabled if this action is valid
 
-		}
-		elseif ( 'delete-endpoint' == $action) {
-			if ( $id = (int)$this->getPost('id')) {
+		} elseif ('delete-endpoint' == $action) {
+			if ($id = (int)$this->getPost('id')) {
 				$dao = new dao\wepm_event;
-				if ( $dto = $dao->getByID( $id)) {
+				if ($dto = $dao->getByID($id)) {
 					// sys::logger( sprintf('found %s : %s', $dto->locale, __METHOD__));
-					$dao->Q( sprintf( 'DELETE FROM `wepm_event` WHERE `locale` = "%s"', $dao->escape( $dto->locale)));
-
+					$dao->Q(sprintf('DELETE FROM `wepm_event` WHERE `locale` = "%s"', $dao->escape($dto->locale)));
 				}
-				Json::ack( $action);
-
+				Json::ack($action);
+			} else {
+				Json::nak($action);
 			}
-			else {
-				Json::nak( $action);
-
-			}
-
+		} else {
+			parent::postHandler();
 		}
-		else { parent::postHandler(); }
-
 	}
 
-	function __construct( $rootPath ) {
+	function __construct($rootPath) {
 		$this->label = config::$WEBNAME;
 
 		/**
 		 * have to use application wide Request
 		 * - it is not initiated on controller yet
 		 */
-		if ( Request::get()->isPost()) {
+		if (Request::get()->isPost()) {
 			$action = Request::get()->getPost('action');
-			if ( 'wemp-log' == $action) {
+			if ('wemp-log' == $action) {
 				$this->RequireValidation = false;
 				// sys::logger( sprintf('set RequireValidation false : %s', __METHOD__));
 
 			}
-
 		}
 
-		parent::__construct( $rootPath);
-
+		parent::__construct($rootPath);
 	}
 
-	function view( $id = 0) {
-		if ( $id = (int)$id ) {
+	function view($id = 0) {
+		if ($id = (int)$id) {
 			$dao = new dao\wepm_event;
-			if ( $dto = $dao->getByID( $id)) {
+			if ($dto = $dao->getByID($id)) {
 				$this->data = (object)[
 					'dto' => $dto,
 
@@ -149,14 +131,10 @@ class controller extends \Controller {
 				$this->render([
 					'title' => $this->title = 'WEPM Event : view',
 					'primary' => 'event-view',
-					'secondary' =>'index'
+					'secondary' => 'index'
 
 				]);
-
 			}
-
 		}
-
 	}
-
 }
